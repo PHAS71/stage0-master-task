6930c5474bf26969f302e02451d2b8e0f7212c8b
PHAS71 <129070669+PHAS71@users.noreply.github.com>
2023-07-11 22:41:30 +0300

    Add files via upload

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -1,8 +1,6 @@
-package com.epam.langSyntax;
-
 public class DigitsSumCalculator {
-
     public void calculateSum(int number) {
+        int ans = number % 10 + number % 100 / 10 + number % 1000 / 100 + number / 1000;
+        System.out.println(ans);
     }
-
 }
